de21a50e6375c3e8149d4bfb79fec8c51f9fe620
Raccoon Cat <t_112ka@yahoo.co.jp>
2024-11-04 15:15:28 +0900

    add cors config to backend

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"ticket-booking-app/config"
 	"ticket-booking-app/db"
@@ -21,6 +22,12 @@ func main() {
 		AppName:      "チケット予約",
 		ServerHeader: "Fiber",
 	})
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8081",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Content-Type,Authorization",
+}))
 
 	// Repositories
 	authRepository := repositories.NewAuthRepository(db)
