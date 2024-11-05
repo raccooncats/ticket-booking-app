eaae5666cb18f34c05bf860bdd02687f1fa5d496
Raccoon Cat <t_112ka@yahoo.co.jp>
2024-11-05 11:18:24 +0900

    feat: add logout func to setting

--- mobile/app/(authed)/(tabs)/settings.tsx
+++ mobile/app/(authed)/(tabs)/settings.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/Button";
+import { VerticalStack } from "@/components/VerticalStack";
+import { useAuth } from "@/context/AuthContext";
+
 const SettingsScreen = () => {
-  return <></>;
+  const { logout } = useAuth();
+
+  return (
+    <VerticalStack flex={1} m={20}>
+      <Button onPress={logout}>ログアウト</Button>
+    </VerticalStack>
+  );
 };
 
 export default SettingsScreen;
